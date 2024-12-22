227f0fc9398b4e73ec4f02e03c0eb0550800554a
Daniel Powley <danielpowley92@outlook.com>
2024-12-22 19:21:13 +0000

    Minor update

--- azure-virtual-desktop/varibles.tf
+++ azure-virtual-desktop/varibles.tf
@@ -2,3 +2,7 @@ variable "deployment_location" {
     description = "The detaul location for the resource deployment"
     default = "uksouth"
 }
+
+variable "default_VM_SKU" {
+  default = "Standard_B2s"
+}
